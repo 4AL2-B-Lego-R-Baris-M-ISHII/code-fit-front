95468ebbe0115a69f54daae994a29082089c4b83
MasatakaIshii <masaradio78@gmail.com>
2021-05-11 16:49:28 +0200

    basic editor done

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -4,17 +4,32 @@
         <p>
             Futur éditeur
         </p>
+      <div class="element"></div>
     </div>
     <button type='button' @click='logout()'>Se déconnecter</button>
 </template>
 
 <script>
+import { Editor } from '@tiptap/core'
+import StarterKit from '@tiptap/starter-kit'
+
 export default {
   name: 'Editor',
   data () {
     return {
-      authenticated: true
+      authenticated: true,
+      editor: null
     }
+  },
+
+  mounted () {
+    this.editor = new Editor({
+      element: document.querySelector('.element'),
+      extensions: [
+        StarterKit
+      ],
+      content: '<p>Hello World!</p>'
+    })
   },
   methods: {
     logout () {
