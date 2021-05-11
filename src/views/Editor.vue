a4b431a742ae28034af332ca8bf6a6ad4d7b8af8
MasatakaIshii <masaradio78@gmail.com>
2021-05-11 21:01:38 +0200

    Editor to send code to compile done

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,10 +1,21 @@
 <template>
-    <div id="editor">
+    <div class="editor">
         <h1>Editeur</h1>
         <p>
-            Futur éditeur
+            Futur éditeur de code pour l'instant en C
         </p>
-      <div class="element"></div>
+      <section class="editor__section">
+        <div class="editor__section__code">
+          <div class="element"></div>
+        </div>
+        <div>
+          <button class="editor__section__button" @click="compileCode">Valider</button>
+        </div>
+      </section>
+      <section class="result_section">
+        <div>{{output}}</div>
+      </section>
+
     </div>
     <button type='button' @click='logout()'>Se déconnecter</button>
 </template>
@@ -12,39 +23,95 @@
 <script>
 import { Editor } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
+import axios from 'axios'
 
 export default {
   name: 'Editor',
   data () {
     return {
-      authenticated: true,
-      editor: null
+      editor: null,
+      output: ''
     }
   },
-
   mounted () {
     this.editor = new Editor({
       element: document.querySelector('.element'),
       extensions: [
         StarterKit
       ],
-      content: '<p>Hello World!</p>'
+      content: 'Hello World'
     })
+  },
+  beforeUnmount () {
+    this.editor.destroy()
   },
   methods: {
     logout () {
-      this.authenticated = false
       this.$router.replace({ name: 'login' })
+    },
+    async compileCode () {
+      const content = this.editor.getJSON().content
+        .map(element => element.content[0])
+        .filter(element => element.type === 'text')
+        .map(element => element.text)
+        .join('\n')
+      const codeRequest = {
+        content,
+        language: 'C'
+      }
+      const token = sessionStorage.getItem('code-fit-token')
+
+      const headers = {
+        'Access-Control-Allow-Origin': 'true',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+        Authorization: `Bearer ${token}`
+      }
+      try {
+        const response = await axios
+          .post('http://localhost:8080/api/code',
+            codeRequest,
+            {
+              headers
+            }
+          )
+        this.output = response.data.output
+      } catch (e) {
+        console.error(e)
+      }
     }
   }
 }
 </script>
 
-<style scoped>
-    #secure {
-        background-color: #FFFFFF;
-        border: 1px solid #CCCCCC;
-        padding: 20px;
-        margin-top: 10px;
+<style lang="scss" scoped>
+    .editor {
+      background-color: #FFFFFF;
+      border: 1px solid #CCCCCC;
+      padding: 20px;
+      margin-top: 10px;
+
+      &__section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        &__code {
+          margin-bottom: 1rem;
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          padding: 0 5rem;
+          border: gray 1rem;
+          .element {
+            text-align: start;
+            width: 100%;
+
+          }
+        }
+        &__button {
+          margin-bottom: 1rem;
+          background-color: aqua;
+          width: auto;
+        }
+      }
     }
 </style>
